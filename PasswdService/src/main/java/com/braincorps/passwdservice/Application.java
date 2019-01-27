8fe37b2e4688325df502ad79b586077d196dba86
Nancy Agrawal <nagraw5@uic.edu>
2019-01-26 20:33:34 -0600

    add Swagger for API documentation

--- PasswdService/src/main/java/com/braincorps/passwdservice/Application.java
+++ PasswdService/src/main/java/com/braincorps/passwdservice/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Component
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
 	public static void main(String[] args) {
